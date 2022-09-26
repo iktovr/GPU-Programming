3ccdbb68ed251d19de2571feb74a0d55d958e111
iktovr <vikvladbir@mail.ru>
2022-09-26 22:52:49 +0300

    Исправил макрос

--- common/cuda_timer.hpp
+++ common/cuda_timer.hpp
@@ -4,12 +4,12 @@
 	cudaEvent_t __start, __end; \
 	cudaCheck(cudaEventCreate(&__start)); \
 	cudaCheck(cudaEventCreate(&__end)); \
-	cudaCheck(cudaEventRecord(&__start)); \
+	cudaCheck(cudaEventRecord(__start)); \
 
 #define cudaEndTimer(time) \
-	cudaCheck(cudaEventRecord(__stop)); \
-	cudaCheck(cudaEventSynchronize(__stop)); \
-	cudaCheck(cudaEventElapsedTime(&time, __start, __stop)); \
+	cudaCheck(cudaEventRecord(__end)); \
+	cudaCheck(cudaEventSynchronize(__end)); \
+	cudaCheck(cudaEventElapsedTime(&time, __start, __end)); \
 	cudaCheck(cudaEventDestroy(__start)); \
-	cudaCheck(cudaEventDestroy(__stop)); \
+	cudaCheck(cudaEventDestroy(__end)); \
 
