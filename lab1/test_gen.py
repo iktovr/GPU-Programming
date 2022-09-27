82cf2fa8e552fbc9ea926d35d21866d023fc402c
iktovr <vikvladbir@mail.ru>
2022-09-27 09:19:43 +0300

    Переписал без click. Скомпилировал benchmark

--- lab1/test_gen.py
+++ lab1/test_gen.py
@@ -1,20 +1,17 @@
-import click
 from random import randint
 from pathlib import Path
+from sys import argv
 
 
-@click.command()
-@click.option("--start", type=int, default=0)
-@click.option("--stop", type=int, default=1e6)
-@click.argument("tests_dir", type=click.Path(exists=True, dir_okay=True, file_okay=False, path_type=Path))
-@click.argument("counts", type=int, nargs=-1)
-def test_gen(start, stop, tests_dir, counts):
-    for count in counts:
-        with open(tests_dir / f"test{count}", "wt") as test:
-            test.write(f"{count}\n")
-            for i in range(count):
-                test.write(f"{randint(start, stop)} ")
+assert len(argv) >= 3
+tests_dir = Path(argv[1])
+assert tests_dir.is_dir()
+counts = [int(i) for i in argv[2:]]
+start = 0
+stop = 1e6
 
-
-if __name__ == "__main__":
-    test_gen()
+for count in counts:
+    with open(tests_dir / "test{}".format(count), "wt") as test:
+        test.write(str(count) + "\n")
+        for i in range(count):
+            test.write(str(randint(start, stop)) + " ")
