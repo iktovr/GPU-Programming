db8dc1cb855177338f55c10498481c12ab7ca477
iktovr <vikvladbir@mail.ru>
2022-09-27 09:35:00 +0300

    open не принимает Path. benchmark не работает

--- lab1/test_gen.py
+++ lab1/test_gen.py
@@ -11,7 +11,7 @@ start = 0
 stop = 1e6
 
 for count in counts:
-    with open(tests_dir / "test{}".format(count), "wt") as test:
+    with open(str(tests_dir / "test{}".format(count)), "wt") as test:
         test.write(str(count) + "\n")
         for i in range(count):
             test.write(str(randint(start, stop)) + " ")
