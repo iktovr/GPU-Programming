49ef45c00480e578e610dd4c50613cc4044d2dc8
iktovr <vikvladbir@mail.ru>
2022-09-27 17:26:36 +0300

    Переписал с argparse и кастомным print_table

--- benchmark.py
+++ benchmark.py
@@ -1,29 +1,29 @@
-import click
+import argparse
 import sys
 from pathlib import Path
 import subprocess
-import termtables
 
 
-class GridDim(click.ParamType):
-    def convert(self, value, param, ctx):
-        try:
-            _ = [int(i) for i in value.split(' ')]
-        except Exception as e:
-            self.fail(str(e), param, ctx)
-        else:
-            return value.split(' ')
+def print_table(table, headers=None, hsep='-', vsep='|'):
+    assert headers is None or len(table[0]) == len(headers)
+
+    column_width = []
+    for i, col in enumerate(zip(*table)):
+        column_width.append(max([len(str(i)) for i in col]))
+        if headers is not None:
+            column_width[-1] = max(column_width[-1], len(str(headers[i])))
+
+    if headers is not None:
+        print(vsep, vsep.join([' {:%d} ' % i for i in column_width]).format(*headers), vsep, sep='')
+        print(vsep, vsep.join([hsep * (i + 2) for i in column_width]), vsep, sep='')
+
+    for row in table:
+        print(vsep, vsep.join([' {:%d} ' % i for i in column_width]).format(*row), vsep, sep='')
 
 
-@click.command()
-@click.option("--gpu", type=Path, default=None)
-@click.option("--cpu", type=Path, default=None)
-@click.option("--test", "-t", "single_tests", multiple=True, 
-              type=click.Path(exists=True, path_type=Path))
-@click.option("--test-dir", type=click.Path(exists=True, dir_okay=True, file_okay=False, path_type=Path), default=None)
-@click.argument("kernels", nargs=-1, type=GridDim())
-def benchmark(gpu, cpu, single_tests, test_dir, kernels):
-    tests = list(single_tests)
+def benchmark(gpu, cpu, tests, test_dir, kernels):
+    if tests is None:
+        tests = []
     if test_dir is not None:
         for test in test_dir.iterdir():
             if test.is_file():
@@ -43,10 +43,10 @@ def benchmark(gpu, cpu, single_tests, test_dir, kernels):
                     try:
                         result = subprocess.run([gpu, *kernel], stdin=test, capture_output=True)
                     except OSError as e:
-                        click.echo(f"Execution failed: {e}")
+                        print("Execution failed: %s" % e)
                         sys.exit()
                     if result.returncode != 0:
-                        click.echo(f"{gpu.name} with test {test_file.name} return {result.returncode}")
+                        print("%s with test %s return %d" % (gpu.name, test_file.name, result.returncode))
                         sys.exit()
                     table[j][i+1].append(int(result.stdout))
                 test.seek(0)
@@ -58,15 +58,27 @@ def benchmark(gpu, cpu, single_tests, test_dir, kernels):
                 try:
                     result = subprocess.run([cpu], stdin=test, capture_output=True)
                 except OSError as e:
-                    click.echo(f"Execution failed: {e}")
+                    print("Execution failed: %s" % e)
                     sys.exit()
                 if result.returncode != 0:
-                    click.echo(f"{cpu.name} with test {test_file.name} return {result.returncode}")
+                    print("%s with test %s return %d" % (cpu.name, test_file.name, result.returncode))
                     sys.exit()
                 table[-1].append(int(result.stdout))
 
-    click.echo(termtables.to_string(table, header=headers, style=termtables.styles.markdown))
+    print_table(table, headers=headers)
 
+
+def grid_dim(value):
+    return [str(int(i)) for i in value.split(' ')]
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--gpu", type=Path, default=None)
+parser.add_argument("--cpu", type=Path, default=None)
+parser.add_argument("--tests", "-t", nargs="*", type=Path)
+parser.add_argument("--test-dir", type=Path, default=None)
+parser.add_argument("kernels", nargs="*", type=grid_dim)
 
 if __name__ == "__main__":
-    benchmark()
+    args = parser.parse_args()
+    benchmark(**vars(args))
