1b1c3851812f5199405419487ce6b92ca5d190b6
iktovr <vikvladbir@mail.ru>
2022-09-28 22:32:48 +0300

    Поправил отображение таблицы

--- benchmark.py
+++ benchmark.py
@@ -11,7 +11,7 @@ def print_table(table, headers=None, hsep='-', vsep='|'):
 
     column_width = []
     for i, col in enumerate(zip(*table)):
-        column_width.append(max([len(str(i)) for i in col]))
+        column_width.append(max([len(('{:.6f}' if type(i) is float else '{}').format(i)) for i in col]))
         if headers is not None:
             column_width[-1] = max(column_width[-1], len(str(headers[i])))
 
@@ -20,7 +20,9 @@ def print_table(table, headers=None, hsep='-', vsep='|'):
         print(vsep, vsep.join([hsep * (i + 2) for i in column_width]), vsep, sep='')
 
     for row in table:
-        print(vsep, vsep.join([' {:%d} ' % i for i in column_width]).format(*row), vsep, sep='')
+        print(vsep, 
+              vsep.join([(' {:%d.6f} ' if type(row[i]) is float else ' {:%d} ') % w for i, w in enumerate(column_width)]).format(*row), 
+              vsep, sep='')
 
 
 def benchmark(gpu, cpu, tests, test_dir, kernels, repeat):
@@ -50,6 +52,7 @@ def benchmark(gpu, cpu, tests, test_dir, kernels, repeat):
                         print("%s with test %s return %d" % (cpu.name, test_file.name, result.returncode))
                         sys.exit()
                     time[i] = float(result.stdout)
+                    test.seek(0)
                 table[-1].append(sum(time) / repeat)
 
     if gpu is not None:
@@ -60,7 +63,7 @@ def benchmark(gpu, cpu, tests, test_dir, kernels, repeat):
         for i, test_file in enumerate(tests):
             with open(str(test_file), 'r') as test:
                 for j, kernel in enumerate(kernels):
-                    for i in range(repeat):
+                    for k in range(repeat):
                         try:
                             result = subprocess.run([str(gpu), *kernel], stdin=test, stdout=subprocess.PIPE)
                         except OSError as e:
@@ -69,9 +72,9 @@ def benchmark(gpu, cpu, tests, test_dir, kernels, repeat):
                         if result.returncode != 0:
                             print("%s with test %s return %d" % (gpu.name, test_file.name, result.returncode))
                             sys.exit()
-                        time[i] = float(result.stdout)
+                        time[k] = float(result.stdout)
+                        test.seek(0)
                     table[j+1][i+1] = sum(time) / repeat
-                test.seek(0)
 
     print_table(table, headers=headers)
 
--- lab1/test.sh
+++ lab1/test.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash
-./test_gen.py tests 1000 10000 100000 1000000 10000000
 ../benchmark.py --gpu bench/lab1_gpu --cpu bench/lab1_cpu -r 5 --test-dir tests "1 32" "32 32" "64 64" "128 128" "256 256" "512 512" "1024 1024"
