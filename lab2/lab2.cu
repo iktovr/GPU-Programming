970bdc876a49c9617c67cc9d285f7eb9ed4550e4
iktovr <vikvladbir@mail.ru>
2022-10-08 15:53:54 +0300

    cpu версия, предварительный вариант cuda версии, тестовые изображения

---
+++ lab2/lab2.cu
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+#include <fstream>
+
+#include "../common/error_checkers.hpp"
+
+__constant__ char Wx[3][3], Wy[3][3];
+
+__global__ sobel_filter(cudaArray_t img, uchar4 *res, int width, int height) {
+	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+	int idy = blockDim.y * blockIdx.y + threadIdx.y;
+	int offsetx = blockDim.x * gridDim.x;
+	int offsety = blockDim.y * gridDim.y;
+
+	double Gx, Gy, grad, lum;
+	uchar4 p;
+	for(int y = idy; y < height; y += offsety)
+		for(int x = idx; x < width; x += offsetx) {
+			Gx = 0;
+			Gy = 0;
+			for (int u = -1; u <= 1; ++u) {
+				for (int v = -1; v <= 1; ++v) {
+					p = tex2D(tex, x + u, y + v);
+					lum = 0.299 * p.x +  0.587 * p.y + 0.114 * p.z;
+					Gx += Wx[u+1][v+1] * lum;
+					Gy += Wy[u+1][v+1] * lum;
+				}
+			}
+			grad = min(255., sqrt(Gx * Gx + Gy * Gy));
+			res[y * width + x] = make_uchar4(grad, grad, grad, p.w);
+		}
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+
+	char host_Wx[3][3] = {
+		{-1, 0, 1},
+		{-2, 0, 2},
+		{-1, 0, 1}
+	},
+	host_Wy[3][3] = {
+		{-1, -2, -1},
+		{ 0,  0,  0},
+		{ 1,  2,  1}
+	};
+	cudaCheck(cudaMemcpyToSymbol(Wx, host_Wx), 9);
+	cudaCheck(cudaMemcpyToSymbol(Wy, host_Wy), 9);
+
+	std::string in_filename, out_filename;
+	std::cin >> in_filename >> out_filename;
+
+	int width, height;
+	std::ifstream in_file(in_filename, std::ios::binary);
+	check(in_file.is_open(), "failed to open input file", false);
+
+	in_file.read(reinterpret_cast<char*>(&width), sizeof(width));
+	in_file.read(reinterpret_cast<char*>(&height), sizeof(height));
+
+	std::vector<uchar4> img(width * height);
+	img.shrink_to_fit();
+	in_file.read(reinterpret_cast<char*>(img.data()), sizeof(uchar4) * width * height);
+
+	cudaArray_t dev_img;
+	cudaChannelFormatDesc ch_desc = cudaCreateChannelDesc<uchar4>();
+	cudaCheck(cudaMallocArray(&dev_img, &ch_desc, width, height));
+	cudaCheck(cudaMemcpy2DToArray(dev_img, 0, 0, img.data(), width * sizeof(uchar4), 
+		                          width * sizeof(uchar4), height, cudaMemcpyHostToDevice));
+
+	struct cudaResourceDesc res_desc;
+	memset(&res_desc, 0, sizeof(res_desc));
+	res_desc.decs = ch_desc;
+	res_desc.resType = cudaResourceTypeArray;
+	res_desc.res.array.array = dev_img;
+
+	struct cudaTextureDesc tex_desc;
+	memset(&tex_desc, 0, sizeof(tex_desc));
+	tex_desc.addressMode[0] = cudaAddressModeClamp;
+	tex_desc.addressMode[1] = cudaAddressModeClamp;
+	tex_desc.filterMode = cudaFilterModePoint;
+	tex_desc.readMode = cudaReadModeElementType;
+	tex_desc.normalizedCoords = false;
+
+	cudaTextureObject_t tex_obj = 0;
+    cudaCheck(cudaCreateTextureObject(&tex_obj, &res_desc, &tex_desc, NULL));
+
+    uchar4 *dev_res;
+    cudaCheck(cudaMalloc(&dev_res, width * height * sizeof(uchar4)));
+
+    sobel_filter<<<dim3(16, 16), dim3(32, 32)>>>(dev_img, dev_res, width, height);
+
+	std::vector<uchar4> res(width * height);
+	res.shrink_to_fit();
+	cudaCheck(cudaMemcpy(res.data(), dev_res, sizeof(uchar4) * width * height, cudaMemcpyDeviceToHost));
+
+	std::ofstream out_file(out_filename, std::ios::binary);
+	check(out_file.is_open(), "failed to open file", false);
+
+	out_file.write(reinterpret_cast<char*>(&width), sizeof(width));
+	out_file.write(reinterpret_cast<char*>(&height), sizeof(height));
+	out_file.write(reinterpret_cast<char*>(res.data()), sizeof(uchar4) * width * height);
+
+	cudaCheck(cudaDestroyTextureObject(tex_bj));
+	cudaCheck(cudaFreeArray(dev_img));
+	cudaCheck(cudaFree(dev_res));
+	return 0;
+}
---
+++ lab2/lab2_cpu.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+#ifdef STBI
+#define STB_IMAGE_IMPLEMENTATION
+#include "../external/stb/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../external/stb/stb_image_write.h"
+
+#else
+#include <fstream>
+#endif
+
+#ifdef TIME
+#include <chrono>
+using namespace std::chrono;
+#endif
+
+#include "../common/error_checkers.hpp"
+
+using std::min;
+using std::max;
+using std::sqrt;
+
+double rgb_to_luminance(uint8_t r, uint8_t g, uint8_t b) {
+	return 0.299 * r +  0.587 * g + 0.114 * b;
+}
+
+void sobel_filter(uint8_t *img, int width, int height, int channels, uint8_t *res, int8_t Wx[][3], int8_t Wy[][3]) {
+	double Gx, Gy, grad;
+	for (int i = 0; i < width; ++i) {
+		for (int j = 0; j < height; ++j) {
+			Gx = 0;
+			Gy = 0;
+			for (int u = -1; u <= 1; ++u) {
+				for (int v = -1; v <= 1; ++v) {
+					int ip = max(min(i + u, width-1), 0),
+					    jp = max(min(j + v, height-1), 0);
+					double lum = rgb_to_luminance(img[(jp * width + ip) * channels],
+					                             img[(jp * width + ip) * channels + 1],
+					                             img[(jp * width + ip) * channels + 2]);
+					Gx += Wx[u+1][v+1] * lum;
+					Gy += Wy[u+1][v+1] * lum;
+				}
+			}
+			grad = min(255., sqrt(Gx * Gx + Gy * Gy));
+
+			res[(j * width + i) * channels] = static_cast<uint8_t>(grad);
+			res[(j * width + i) * channels + 1] = static_cast<uint8_t>(grad);
+			res[(j * width + i) * channels + 2] = static_cast<uint8_t>(grad);
+			res[(j * width + i) * channels + 3] = img[(j * width + i) * channels + 3];
+		}
+	}
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::string in_filename, out_filename;
+	std::cin >> in_filename >> out_filename;
+
+	int width, height, channels;
+	uint8_t *img;
+
+#ifdef STBI
+	img = stbi_load(in_filename.c_str(), &width, &height, &channels, 0);
+	check(img, "error loading image", NULL);
+#else
+	std::ifstream in_file(in_filename, std::ios::binary);
+	check(in_file.is_open(), "failed to open file", false);
+
+	in_file.read(reinterpret_cast<char*>(&width), sizeof(width));
+	in_file.read(reinterpret_cast<char*>(&height), sizeof(height));
+	channels = 4;
+
+	std::vector<uint8_t> imgv(width * height * channels);
+	imgv.shrink_to_fit();
+	in_file.read(reinterpret_cast<char*>(imgv.data()), width * height * channels);
+	img = imgv.data();
+#endif
+
+	int8_t Wx[3][3] = {
+		{-1, 0, 1},
+		{-2, 0, 2},
+		{-1, 0, 1}
+	},
+	Wy[3][3] = {
+		{-1, -2, -1},
+		{ 0,  0,  0},
+		{ 1,  2,  1}
+	};
+
+	std::vector<uint8_t> res(width * height * channels);
+	res.shrink_to_fit();
+
+#ifdef TIME
+	steady_clock::time_point start = steady_clock::now();
+#endif
+
+	sobel_filter(img, width, height, channels, res.data(), Wx, Wy);
+
+#ifdef TIME
+	steady_clock::time_point end = steady_clock::now();
+	std::cout << duration_cast<nanoseconds>(end - start).count() / 1000000.0;
+#else
+
+#ifdef STBI
+	stbi_write_png(out_filename.c_str(), width, height, channels, res.data(), width * channels);
+#else
+	std::ofstream out_file(out_filename, std::ios::binary);
+	check(out_file.is_open(), "failed to open file", false);
+
+	out_file.write(reinterpret_cast<char*>(&width), sizeof(width));
+	out_file.write(reinterpret_cast<char*>(&height), sizeof(height));
+	out_file.write(reinterpret_cast<char*>(res.data()), width * height * channels);
+#endif
+#endif
+
+#ifdef STBI
+	stbi_image_free(img);
+#endif
+	return 0;
+}
