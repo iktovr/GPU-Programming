eeeb87d9568b1091c30e4a0b4b4a4b711a8dd20a
iktovr <vikvladbir@mail.ru>
2022-10-12 17:16:05 +0300

    Add lab2

--- common/error_checkers.hpp
+++ common/error_checkers.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
 
+#ifdef __CUDACC__
+
 #define cudaCheck(val) __cudaCheck((val), __FILE__, __LINE__)
 
 inline void __cudaCheck(cudaError_t err, const char *const file, const int line) {
@@ -13,8 +15,10 @@ inline void __cudaCheck(cudaError_t err, const char *const file, const int line)
 
 #define cudaCheckLastError() __cudaCheck(cudaGetLastError(), __FILE__, __LINE__)
 
-#define check(value, msg, err_value) \
-if (value == err_value) { \
-	std::cerr << "ERROR: " << msg << " at " << __FILE__ << ":" << __LINE__ << '\n'; \
+#endif
+
+#define check(value, err_value, msg) \
+if ((value) == (err_value)) { \
+	std::cerr << "ERROR: " << (msg) << " at " << __FILE__ << ":" << __LINE__ << '\n'; \
 	exit(0); \
 }
---
+++ lab2/test_gen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Написан для python 3.5
+
+from random import randrange
+from pathlib import Path
+import argparse
+import struct
+
+
+def test_gen(resolution, tests_dir):
+    assert tests_dir.is_dir() or not tests_dir.exists()
+    
+    if not tests_dir.exists():
+        tests_dir.mkdir()
+    for width, height in [map(int, res.split('x')) for res in resolution]:
+        testname = Path("test%dx%d.data" % (width, height))
+        with open(str(tests_dir / testname), "wb") as test:
+            test.write(struct.pack('ii', width, height))
+            for _ in range(width * height):
+                test.write(struct.pack('BBBB', randrange(256), randrange(256), randrange(256), randrange(256)))
+        with open(str(tests_dir / testname.with_suffix('.in')), 'wt') as test:
+            test.write(str(tests_dir / testname) + '\n' + str(tests_dir / testname.with_suffix('.out')))
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("tests_dir", type=Path)
+parser.add_argument("resolution", type=str, nargs="*", help="resolutions in format WIDTHxHEIGHT")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    test_gen(**vars(args))
